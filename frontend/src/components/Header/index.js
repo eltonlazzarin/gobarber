b94627d5e0833fd3bcb2b8222c0dcabdd82cbdb4
eltonlazzarin <elton.lazzarin@outlook.com>
2019-10-17 20:59:47 -0300

    gobarber frontend

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import Notifications from '~/components/Notifications';
+
+import logo from '~/assets/logo-purple.svg';
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Notifications />
+
+          <Profile>
+            <div>
+              <strong>{profile.name}</strong>
+              <Link to="/profile">My profile</Link>
+            </div>
+            <img src={profile.avatar.url} alt="Elton Lazzarin" />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
---
+++ frontend/src/pages/SignUp/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import logo from '~/assets/logo.svg';
+
+import { signUpRequest } from '~/store/modules/auth/actions';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Name is required'),
+  email: Yup.string()
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: Yup.string()
+    .min(6, 'Password must contain at least 6 characters')
+    .required('Password is required'),
+});
+
+export default function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Your full name" />
+        <Input name="email" type="email" placeholder="Your email" />
+        <Input name="password" type="password" placeholder="Your password" />
+
+        <button type="submit">Create account</button>
+        <Link to="/">I already have account</Link>
+      </Form>
+    </>
+  );
+}
